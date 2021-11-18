58c8ebd6b70897d9a086da18512945176843c4af
ht0m <hartht@gmail.com>
2021-11-18 16:27:05 +0100

    Hangman

---
+++ src/main/java/day03/hangman/HangmanMain.java
@@ -0,0 +1,45 @@
+package day03.hangman;
+
+import java.util.Scanner;
+
+    public class HangmanMain {
+
+        public static void main(String[] args) {
+            new day03.hangman.HangmanMain().run();
+        }
+
+        private void run() {
+            String wordToFind = "alma";
+            String wordFound = "____";
+            int chances = 8;
+            Scanner scanner = new Scanner(System.in);
+            do {
+                System.out.println("Itt tartasz: " + wordFound);
+                System.out.println("Ennyi tipped van: " + chances);
+                System.out.println("Mi a következő tipp? ");
+                String guess = scanner.nextLine();
+                if (wordToFind.contains(guess)) {
+                    System.out.println("jó betű");
+                    wordFound = updateWordFound(wordToFind, wordFound, guess);
+                }  else {
+                    chances --;
+                }
+            } while (chances>0 && !wordToFind.equals(wordFound));
+
+            if (wordToFind.equals(wordFound)) {
+                System.out.println("ügyes");
+            } else {
+                System.out.println("sajnos vesztettél");
+            }
+        }
+
+        private String updateWordFound(String wordToFind, String status, String guess) {
+            char[] chars = status.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (wordToFind.charAt(i) == guess.charAt(0)) {
+                    chars[i] = guess.charAt(0);
+                }
+            }
+            return new String(chars);
+        }
+}
