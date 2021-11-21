169cd467623711da23fa3c6fc274959aed9335a8
ht0m <hartht@gmail.com>
2021-11-21 13:43:05 +0100

    PerfectNumbers

---
+++ src/main/java/day02/PerfectNumbers.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class PerfectNumbers {
+
+    public boolean isPerfectNumber(int number) {
+        int sumOfDivider = 0;
+        for (int i = 1; i <= number/2; i++) {
+            if (number % i == 0) {
+                sumOfDivider += i;
+            }
+        }
+        return (sumOfDivider == number) ? true: false;
+    }
+}
